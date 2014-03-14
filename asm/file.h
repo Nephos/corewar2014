557e19af3fafc0c2e64692b7170573c985d7b1c5
poulet_a <poulet_a@epitech.eu>
2014-03-14 19:27:43 +0100

    Asm files - Add header file.h - begin main

---
+++ asm/file.h
@@ -0,0 +1,17 @@
+/*
+** file.h for  in /home/poulet_a/projets/corewar/asm
+** 
+** Made by poulet_a
+** Login   <poulet_a@epitech.net>
+** 
+** Started on  Fri Mar 14 19:26:13 2014 poulet_a
+** Last update Fri Mar 14 19:27:34 2014 poulet_a
+*/
+
+#ifndef FILE_H_
+# define FILE_H_
+
+int	asm_open_s_file(char *name);
+int	asm_open_cor_file(char *name);
+
+#endif /* !FILE_H_ */
--- asm/main.c
+++ asm/main.c
@@ -5,13 +5,18 @@
 ** Login   <poulet_a@epitech.net>
 ** 
 ** Started on  Tue Mar 11 08:22:38 2014 poulet_a
-** Last update Fri Mar 14 18:37:39 2014 poulet_a
+** Last update Fri Mar 14 19:26:03 2014 poulet_a
 */
 
 #include <stdlib.h>
 #include "my.h"
+#include "file.h"
 
 int     main(void)
 {
+  int	fd[2];
+
+  fd[0] = asm_open_s_file("asm.s");
+  fd[1] = asm_open_cor_file("asm.cor");
   return (0);
 }
