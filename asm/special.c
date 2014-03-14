c1ae1ef28091112dc0e8340ac00eea1902e5cf05
poulet_a <poulet_a@epitech.eu>
2014-03-14 13:05:36 +0100

    Asm : add special.c
    - Special : Add is_comment to know if the line is a comment

---
+++ asm/special.c
@@ -0,0 +1,17 @@
+/*
+** special.c for  in /home/poulet_a/projets/corewar/asm
+** 
+** Made by poulet_a
+** Login   <poulet_a@epitech.net>
+** 
+** Started on  Fri Mar 14 13:03:39 2014 poulet_a
+** Last update Fri Mar 14 13:05:25 2014 poulet_a
+*/
+
+#include <stdlib.h>
+#include "special.h"
+
+int	spe_is_comment(char *str)
+{
+  return (my_match(str, "#*"));
+}
---
+++ asm/special.h
@@ -0,0 +1,16 @@
+/*
+** special.h for  in /home/poulet_a/projets/corewar/asm
+** 
+** Made by poulet_a
+** Login   <poulet_a@epitech.net>
+** 
+** Started on  Fri Mar 14 13:03:36 2014 poulet_a
+** Last update Fri Mar 14 13:05:33 2014 poulet_a
+*/
+
+#ifndef SPECIAL_H_
+# define SPECIAL_H_
+
+int	spe_is_comment(char *str);
+
+#endif /* !SPECIAL_H_ */
