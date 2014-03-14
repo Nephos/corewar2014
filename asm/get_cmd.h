429772d0be47f0d351ba1405af44ccb4a790d363
poulet_a <poulet_a@epitech.eu>
2014-03-14 21:12:14 +0100

    Asm
    Add header for get_cmd

---
+++ asm/get_cmd.h
@@ -0,0 +1,17 @@
+/*
+** get_cmd.h for  in /home/poulet_a/projets/corewar/asm
+** 
+** Made by poulet_a
+** Login   <poulet_a@epitech.net>
+** 
+** Started on  Fri Mar 14 21:11:15 2014 poulet_a
+** Last update Fri Mar 14 21:12:09 2014 poulet_a
+*/
+
+#ifndef GET_CMD_H_
+# define GET_CMD_H_
+
+static int	spe_is_comment(char *str);
+char		*asm_get_next_cmd(int fd);
+
+#endif /* !GET_CMD_H_ */
