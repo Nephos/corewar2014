4be0568bd9da8817f2154dfc9a8eb138ee1c1abd
poulet_a <poulet_a@epitech.eu>
2014-03-19 15:11:08 +0100

    Asm
    Add little doc and fix header

--- asm/label.h
+++ asm/label.h
@@ -5,7 +5,7 @@
 ** Login   <poulet_a@epitech.net>
 ** 
 ** Started on  Fri Mar 14 13:06:51 2014 poulet_a
-** Last update Wed Mar 19 14:59:14 2014 poulet_a
+** Last update Wed Mar 19 15:11:05 2014 poulet_a
 */
 
 #ifndef LABEL_H_
@@ -13,10 +13,20 @@
 
 # include "list.h"
 
+/*
+** Basic manipulating function of label
+*/
 int	lab_size(t_lab *list);
 t_lab	*lab_new(char *name, int addr, t_lab *next, t_lab *prev);
 t_lab	*lab_create(t_lab *list, char *name, int addr);
 t_lab	*lab_create_at_begin(t_lab *list, char *name, int addr);
 t_lab	*lab_last(t_lab *list);
+
+/*
+** Management of label str <-> label,
+** research
+*/
+int	lab_str_valid(char *name);
+int	lab_get_addr(char *str);
 
 #endif /* !LABEL_H_ */
