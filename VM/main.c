f3dc2fb6720fca6de84630c75d716eaf3d169afe
chapui_s <chapui_s@epitech.eu>
2014-03-20 14:46:23 +0100

    test -> first commit

---
+++ VM/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for Corewar in /home/chapui_s/travaux/Corewar/VM
+**
+** Made by chapui_s
+** Login   <chapui_s@epitech.eu>
+**
+** Started on  Thu Mar 20 14:45:46 2014 chapui_s
+** Last update Thu Mar 20 14:45:58 2014 chapui_s
+*/
+
+int		main(int argc, char **argv)
+{
+  return (0);
+}
